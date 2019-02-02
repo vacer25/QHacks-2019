3102e8734c575ee347bbc0e0141c72b36736d470
Troy Giorshev <troygiorshev@gmail.com>
2019-02-02 13:48:37 -0500

    Complete read implementation.  Next TODO writing

--- solidityLearn/contracts/Election.sol
+++ solidityLearn/contracts/Election.sol
@@ -18,15 +18,29 @@ contract Election {
     uint public candidatesCount;
     // You CAN'T iterate over a mapping, so you need to keep track externally
 
-    function addCandidate (string _name) private {
+    function addCandidate (string memory _name) private {
         candidatesCount ++;
         candidates[candidatesCount] = Candidate(candidatesCount, _name, 0);
     }
+    // This is in memory because it's just a temporary thing.  We would say storage if we wanted it written to the blockchain.
+    // For this though, just a temporary parameter so we should be good 
 
     //Constructor
     constructor() public {
         addCandidate("Candidate 1");
         addCandidate("Candidate 2");
     }
-    
+
 }
+
+// truffle console
+// Election.deployed().then(function(i) {app = i})
+// app.candidates()
+    // That expects a key.  Pass it 1 or 2.
+    // If you pass it something wrong, it gives a default value
+// app.candidatesCount()
+// app.candidates(1).then(function(c) {candidate = c;})
+// candidate
+// Then index it's stuff with [0] or ['id]
+// And can do stuff like .toNumber() to get better data
+// Explanation at 1:09:00 with connecting metamask and stuff
