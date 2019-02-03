5ae2633908269705a99bddc6a880ba234452b6a1
Troy Giorshev <troygiorshev@gmail.com>
2019-02-02 20:05:33 -0500

    Proof of concept display working, python lock script functional

--- solidityLearn/src/js/app.js
+++ solidityLearn/src/js/app.js
@@ -2,6 +2,7 @@ App = {
   web3Provider: null,
   contracts: {},
   account: '0x0',
+  assetsShown: {}, // Dict keeping track of which assets have been shown
 
   init: function() {
     return App.initWeb3();
@@ -21,7 +22,7 @@ App = {
   },
 
   render: function() {
-    var electionInstance;
+    var sharedAssetsInstance;
     var loader = $("#loader");
     var content = $("#content");
   
@@ -37,37 +38,46 @@ App = {
     });
   
     // Load contract data
-    App.contracts.Election.deployed().then(function(instance) {
-      electionInstance = instance;
-      return electionInstance.candidatesCount();
-    }).then(function(candidatesCount) {
-      var candidatesResults = $("#candidatesResults");
-      candidatesResults.empty();
+    App.contracts.SharedAssets.deployed().then(function(instance) {
+      saInstance = instance;
+      return saInstance.assetCount();
+    }).then(function(assetCount) {
+      var assetsAvailable = $("#assetsResults");
+      assetsAvailable.empty();
   
-      var candidatesSelect = $('#candidatesSelect');
-      candidatesSelect.empty();
+      var assetSelect = $('#assetSelect');
+      assetSelect.empty();
+
+      App.assetsShown = {} // We just erased all assets off the screen
   
-      for (var i = 1; i <= candidatesCount; i++) {
-        electionInstance.candidates(i).then(function(candidate) {
-          var id = candidate[0];
-          var name = candidate[1];
-          var voteCount = candidate[2];
+      for (var i = 1; i <= assetCount; i++) {
+        saInstance.assets(i).then(function(asset) {
+          // if the asset has been shown, don't show it
+          if (App.assetsShown[i]) return;
+
+          // indicate that we're showing the asset
+          App.assetsShown[i] = true;
+          var id = asset[0];
+          var name = asset[1];
+          var cost = asset[2];
+          var hasOut = asset[3];
+          var checkTime = asset[4];
   
-          // Render candidate Result
-          var candidateTemplate = "<tr><th>" + id + "</th><td>" + name + "</td><td>" + voteCount + "</td></tr>"
-          candidatesResults.append(candidateTemplate);
+          // Render assets
+          var assetTemplate = 
+            "<tr><th>" + name + 
+            "</th><td>" + cost/(1e18) + 
+            "</td><td>" + hasOut + 
+            "</td><td>" + new Date(checkTime*1000) + "</td></tr>";
+          assetsAvailable.append(assetTemplate);
   
-          // Render candidate ballot option
-          var candidateOption = "<option value='" + id + "' >" + name + "</ option>"
-          candidatesSelect.append(candidateOption);
+          // Render asset options
+          var assetOption = "<option value='" + id + "' >" + name + "</ option>";
+          assetSelect.append(assetOption);
         });
       }
-      return electionInstance.voters(App.account);
-    }).then(function(hasVoted) {
-      // Do not allow a user to vote
-      if(hasVoted) {
-        $('form').hide();
-      }
+      // The following from the election example code
+      //return electionInstance.voters(App.account);
       loader.hide();
       content.show();
     }).catch(function(error) {
@@ -75,38 +85,59 @@ App = {
     });
   },
 
-  castVote: function() {
-    var candidateId = $('#candidatesSelect').val();
-    App.contracts.Election.deployed().then(function(instance) {
-      return instance.vote(candidateId, { from: App.account });
+  
+  checkOut: function() {
+    var assetId = $('#assetSelect').val();
+    App.contracts.SharedAssets.deployed().then(function(instance) {
+      return instance.checkOutAsset.sendTransaction(assetId, Math.round((new Date()).getTime() / 1000), { from: App.account });
     }).then(function(result) {
-      // Wait for votes to update
+      // Wait for assets to update
       $("#content").hide();
       $("#loader").show();
     }).catch(function(err) {
       console.error(err);
     });
   },
-
+  
+  checkIn: function() {
+    var assetId = $('#assetSelect').val();
+    App.contracts.SharedAssets.deployed().then(function(instance) {
+      
+      instance.assets(assetId).then(function(asset) {
+        var cost = asset[2];
+        console.log("cost", cost);
+      });
+      
+      return instance.checkInAsset(assetId, Math.round((new Date()).getTime() / 1000), { from: App.account });
+    }).then(function(result) {
+      // Wait for assets to update
+      $("#content").hide();
+      $("#loader").show();
+    }).catch(function(err) {
+      console.error(err);
+    });
+  },
+  
   listenForEvents: function() {
-    App.contracts.Election.deployed().then(function(instance) {
-      instance.votedEvent({}, {
-        fromBlock: 0,
-        toBlock: 'latest'
+    App.contracts.SharedAssets.deployed().then(function(instance) {
+      instance.changedEvent({}, {
+        // Commented out so that we don't show every event
+        //fromBlock: 0,
+        //toBlock: 'latest'
       }).watch(function(error, event) {
         console.log("event triggered", event)
-        // Reload when a new vote is recorded
+        // Reload when something changes
         App.render();
       });
     });
   },
 
   initContract: function() {
-    $.getJSON("Election.json", function(election) {
+    $.getJSON("SharedAssets.json", function(sharedAssets) {
       // Instantiate a new truffle contract from the artifact
-      App.contracts.Election = TruffleContract(election);
+      App.contracts.SharedAssets = TruffleContract(sharedAssets);
       // Connect provider to interact with contract
-      App.contracts.Election.setProvider(App.web3Provider);
+      App.contracts.SharedAssets.setProvider(App.web3Provider);
   
       App.listenForEvents();
   
