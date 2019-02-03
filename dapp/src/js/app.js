e16438b98947e6a1ace775013f9e1a9bf6e64c63
Troy Giorshev <troygiorshev@gmail.com>
2019-02-03 02:55:19 -0500

    Nothing

---
+++ dapp/src/js/app.js
@@ -0,0 +1,151 @@
+App = {
+  web3Provider: null,
+  contracts: {},
+  account: '0x0',
+  assetsShown: {}, // Dict keeping track of which assets have been shown
+
+  init: function() {
+    return App.initWeb3();
+  },
+
+  initWeb3: function() {
+    if (typeof web3 !== 'undefined') {
+      // If a web3 instance is already provided by Meta Mask.
+      App.web3Provider = web3.currentProvider;
+      web3 = new Web3(web3.currentProvider);
+    } else {
+      // Specify default instance if no web3 instance provided
+      App.web3Provider = new Web3.providers.HttpProvider('http://localhost:7545');
+      web3 = new Web3(App.web3Provider);
+    }
+    return App.initContract();
+  },
+
+  render: function() {
+    var sharedAssetsInstance;
+    var loader = $("#loader");
+    var content = $("#content");
+  
+    loader.show();
+    content.hide();
+  
+    // Load account data
+    web3.eth.getCoinbase(function(err, account) {
+      if (err === null) {
+        App.account = account;
+        $("#accountAddress").html("Your Account: " + account);
+      }
+    });
+  
+    // Load contract data
+    App.contracts.SharedAssets.deployed().then(function(instance) {
+      saInstance = instance;
+      return saInstance.assetCount();
+    }).then(function(assetCount) {
+      var assetsAvailable = $("#assetsResults");
+      assetsAvailable.empty();
+  
+      var assetSelect = $('#assetSelect');
+      assetSelect.empty();
+
+      App.assetsShown = {};
+
+      for (var i = 1; i <= assetCount; i++) {
+        saInstance.assets(i).then(function(asset) {
+
+          var id = asset[0];
+          var name = asset[1];
+          var cost = asset[2];
+          var hasOut = asset[3];
+          var checkTime = asset[4];
+
+          if (App.assetsShown[id]) return;
+          
+          App.assetsShown[id] = true;
+
+          var timeStr = checkTime > 10 ? new Date(checkTime*1000).toLocaleString() : "-";
+
+          var outBy = hasOut != 0 ? hasOut : "Item is In"
+  
+          // Render assets
+          var assetTemplate = 
+            "<tr><th>" + name + 
+            "</th><td>" + (cost/(1e18)).toFixed(2) + 
+            "</td><td>" + outBy + 
+            "</td><td>" + timeStr + "</td></tr>";
+          assetsAvailable.append(assetTemplate);
+  
+          // Render asset options
+          var assetOption = "<option value='" + id + "' >" + name + "</ option>";
+          assetSelect.append(assetOption);
+        });
+      }
+      loader.hide();
+      content.show();
+    }).catch(function(error) {
+      console.warn(error);
+    });
+  },
+  
+  checkOut: function() {
+    var assetId = $('#assetSelect').val();
+    App.contracts.SharedAssets.deployed().then(function(instance) {
+      return instance.checkOutAsset.sendTransaction(assetId, Math.round((new Date()).getTime() / 1000), { from: App.account });
+    }).then(function(result) {
+      // Wait for assets to update
+      $("#content").hide();
+      $("#loader").show();
+    }).catch(function(err) {
+      console.error(err);
+    });
+  },
+  
+  checkIn: function() {
+    var assetId = $('#assetSelect').val();
+    App.contracts.SharedAssets.deployed().then(function(instance) {
+      
+      instance.assets(assetId).then(function(asset) {
+        var cost = asset[2];
+        console.log("cost", cost);
+      });
+      
+      return instance.checkInAsset(assetId, Math.round((new Date()).getTime() / 1000), { from: App.account });
+    }).then(function(result) {
+      // Wait for assets to update
+      $("#content").hide();
+      $("#loader").show();
+    }).catch(function(err) {
+      console.error(err);
+    });
+  },
+  
+  listenForEvents: function() {
+    App.contracts.SharedAssets.deployed().then(function(instance) {
+      instance.changedEvent({}, {
+      }).watch(function(error, event) {
+        console.log("event triggered", event)
+        // Reload when something changes
+        App.render();
+      });
+    });
+  },
+
+  initContract: function() {
+    $.getJSON("SharedAssets.json", function(sharedAssets) {
+      // Instantiate a new truffle contract from the artifact
+      App.contracts.SharedAssets = TruffleContract(sharedAssets);
+      // Connect provider to interact with contract
+      App.contracts.SharedAssets.setProvider(App.web3Provider);
+  
+      App.listenForEvents();
+  
+      return App.render();
+    });
+  }
+};
+
+$(function() {
+  $(window).load(function() {
+    App.init();
+  });
+});
